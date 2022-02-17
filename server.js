55800d3b58b462c98d1a64ef91d6dea726d88268
unknown <navoiy498@gmail.com>
2022-02-17 16:58:52 +0500

    final test

--- server.js
+++ server.js
@@ -7,13 +7,20 @@ require("dotenv").config();
 const form_id = uuid.v4();
 
 const crm = new AmoCRM({
-  domain: "abdukarimmirzayev.amocrm.ru",
+  // логин пользователя в портале, где адрес портала domain.amocrm.ru
+  domain: "abdukarimmirzayev.amocrm.ru", // может быть указан полный домен вида domain.amocrm.ru, domain.amocrm.com
+  /* 
+      Информация об интеграции (подробности подключения 
+      описаны на https://www.amocrm.ru/developers/content/oauth/step-by-step)
+    */
   auth: {
-    login: "diyor.amirzayev@gmail.com",
-    hash: "AaDqkjYIRAhgX3zuTfz8tCT3aOOuxW2YhRlxxFOTbKwvNudLf7nJk5YyyoAGdYJ5", // API-ключ доступа
+    client_id: "601050b7-4746-4873-a4d6-d5ccedeaf052", // ID интеграции
+    client_secret:
+      "r5NxL9r8anKFAACkj1ukDfPY7FNswEmdnecSz4sQygWRWCKz0UdDUkh1OpzvW0gM", // Секретный ключ
+    redirect_uri: "https://speachcourse.herokuapp.com/", // Ссылка для перенаправления
+    code: "def5020031cf40dd8d548dceeacd32efd56b53c2b76e2d37de44f3affc0dd778648087d57b83716768c06c873d2cfcf3dbf4221f56d90b8e17d8790dbcda24097b0156b4b5700f99532abee4e29f4f13f9c431366ef611e2540de97a300eefffbf9fba38ffd55f0bbed20281c3c0c7ddd376016d3a74a9e44abb118f2de52d34b050f9f0132e46597a556056d68f3df440f081bd33e5a68ed7a4828b7ca88e96d153db5386a6335f332cc1487eadb5203cc98919a5bdf1bdfbb186115ea8a8de6052effe5c35dec79186446ffd0bfe80692ec3dcefd3fde3f42e27b17ba43ae94544fba8650a43542bcf664b8b2c48fd9285d2b2b151e92e2713b1f90da8622de9ee1b670bf681beb2dff93001a1eaa4058db242adad973a4356cf98f6673a05b0c2c62b7e1c0a2c91d6aaa39b5c2f95fd74c60ded23d36ca355342cabcea5e2fecad279954e8afb64437050cea63318aff5f0a6486c13f5330d2179238cd81333118a39973a362ddc94fa9358b43f3e383d10efc28801b35caaa37e416639e8c4fba85f1453a355239a7173588f4fad0682cbd3de84fd0af2620c1d3c1de0722c614a389b210efd47329a02e771d86866591c2d5a6de9e519b41e2338a60952a190db888a6d7c90423725872024", // Код авторизации
   },
 });
-
 crm
   .connect()
   .then(() => {
@@ -24,7 +31,6 @@ crm
   });
 
 const app = express();
-
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -36,84 +42,17 @@ app.get("/", (req, res) => {
 app.post("/send", (req, res) => {
   var name = req.body.name;
   var tel = req.body.tel;
-
-  const lead = new crm.Lead();
-  lead.name = name;
-  lead.tel = tel;
-  console.log(lead);
-
-  lead.save();
-
-  console.log(lead);
-
-  crm.request
-    .post("/api/v4/leads/unsorted/forms", {
-      request_id: Date.now(),
-      source_name: "node.js heroku",
-      source_uid: "ede127c9-9da1-4e71-aeb1-a5c7ae925603",
-      _embedded: {
-        leads: [
-          {
-            name: "Client",
-            visitor_uid: `${uuid.v4}`,
-            price: 0,
-            custom_fields_values: [
-              {
-                field_id: Date.now(),
-                values: [
-                  {
-                    value: "Field",
-                  },
-                ],
-              },
-            ],
-            _embedded: {
-              tags: [
-                {
-                  name: "order",
-                },
-              ],
-            },
-          },
-        ],
-        contacts: [
-          {
-            name: name,
-            first_name: name,
-            last_name: name,
-            custom_fields_values: [
-              {
-                field_code: "PHONE",
-                values: [
-                  {
-                    value: tel,
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-        companies: [
-          {
-            name: "node.js heroku",
-          },
-        ],
+  const response = crm.request
+    .post("/api/v4/contacts", [
+      {
+        name: `${name}: ${tel}`,
       },
-      metadata: {
-        ip: "123.222.2.22",
-        form_id: form_id,
-        form_sent_at: Date.now(),
-        form_name: "Application form",
-        form_page: "https://example.com",
-        referer: "https://www.google.com/search?&q=elon+musk",
-      },
-    })
+    ])
     .then((data) => {
-      console.log(data);
-      res.status(200).send(data);
+      res.status(200).send("ok");
     })
     .catch((e) => {
-      res.status(401).send("Произошла ошибка создания контакта");
+      res.status(400).send("error");
     });
 });
 
